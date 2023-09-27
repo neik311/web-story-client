0b041b5a3e8cb0710d66fb4f0b50f3b6eca1e16f
nvkien123 <vankienn857@gmail.com>
2023-09-27 21:12:14 +0700

    up env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,16 @@
 export const environment = {
   production: true,
-  authApi: 'http://ape-survey-auth-api.apetechs.co',
-  coreApi: 'http://ape-survey-api.apetechs.co',
+  apiUrl: 'http://localhost:3800',
+  // backEnd: 'http://localhost:3200',
+  // authApi: 'https://dms-pro-auth-api.apetechs.link',
+  // coreApi: 'https://dms-pro-api-core.apetechs.link',
+  firebaseConfig: {
+    apiKey: 'AIzaSyCHsiYO33c3aLh8a1-uZnmc9R_oBg_ZpHw',
+    authDomain: 'web-story-5464b.firebaseapp.com',
+    projectId: 'web-story-5464b',
+    storageBucket: 'web-story-5464b.appspot.com',
+    messagingSenderId: '637118065791',
+    appId: '1:637118065791:web:15493f61c4101dbc98e34a',
+    measurementId: 'G-6P0H4GQBBE',
+  },
 }
